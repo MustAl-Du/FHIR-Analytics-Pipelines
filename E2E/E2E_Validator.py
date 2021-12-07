17379f92dfc0d2ee85962610d9718596f8f48943
QuanWanxx <68055742+QuanWanxx@users.noreply.github.com>
2021-12-07 14:33:10 +0800

    Input storage and sql paras

--- E2E/E2E_Validator.py
+++ E2E/E2E_Validator.py
@@ -1,13 +1,16 @@
 import sys
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--StorageConnectionString", help="Storage connection string.")
+parser.add_argument("--SqlUsername", help="SQL username")
+parser.add_argument("--SqlPassword", help="SQL password")
+args = parser.parse_args()
 
 if __name__ == "__main__":
     sys.stdout.flush()
 
     print('Executing script file is:', str(sys.argv[0]))
     print('The arguments are:', str(sys.argv))
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--world", help="Provide the name of the world to greet.")
-    args = parser.parse_args()
+
     print('Hello ', args.world)
